2b03e99cf38e65142d4857e5453aa3749079a02e
Your GITHUB Name <GITHUB you@example.com>
2018-09-10 10:30:38 +0300

    Initial commit

---
+++ src/dom.java
@@ -0,0 +1,11 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class dom {
+
+    public static void main (String[] args){
+        System.out.print("Загороднюк Олександр Анатолійович\t");
+        System.out.print("Група: AT-161\t");
+        System.out.println("5 в списку");
+        System.out.print("Любить програмировати на Java:)))\n");
+    }
+}
